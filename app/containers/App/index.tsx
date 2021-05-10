2a782820b237c81ff7c396d53fd150e1bbf576f2
alao <alao@marinsoftware.com>
2021-05-09 17:22:08 -0700

    started

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -2,23 +2,12 @@
  *
  * App
  *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
+ * This component is the entry to this App.
  */
 
 import * as React from 'react';
-import { Helmet } from 'react-helmet-async';
 import styled from 'styles/styled-components';
-import { Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
-
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-
-import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
@@ -32,20 +21,7 @@ const AppWrapper = styled.div`
 function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <Footer />
-      <GlobalStyle />
+      <div>Hi Twilio App</div>
     </AppWrapper>
   );
 }
